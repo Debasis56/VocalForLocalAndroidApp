0c8e7512123f55f938cf8bf7ad9c406916b4f4a8
Debasis56 <b519019@iiit-bh.ac.in>
2022-02-20 23:54:13 +0530

    Authentication Service Added

---
+++ app/src/main/java/com/example/android/vocalforlocal/Prevalent/Prevalent.java
@@ -0,0 +1,7 @@
+package com.example.android.vocalforlocal.Prevalent;
+
+import com.example.android.vocalforlocal.Model.Users;
+
+public class Prevalent {
+    private  static Users currentOnlineUser;
+}
