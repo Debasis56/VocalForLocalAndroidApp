532c2c41964c7d38178a0022522bbe99b03822d1
Debasis56 <b519019@iiit-bh.ac.in>
2022-05-17 14:05:13 +0530

    Updated cart and order placed activities and functionalities

--- app/src/main/java/com/example/android/vocalforlocal/CartActivity.java
+++ app/src/main/java/com/example/android/vocalforlocal/CartActivity.java
@@ -23,14 +23,19 @@ import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class CartActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private RecyclerView.LayoutManager layoutManager;
     private Button NextProcessBtn;
-    private TextView txtTotalAmount;
+    private TextView txtTotalAmount, txtMsg1;
+
+    private int overallTotalPrice = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,12 +48,25 @@ public class CartActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(layoutManager);
         NextProcessBtn = (Button) findViewById(R.id.next_process_btn);
         txtTotalAmount = (TextView)findViewById(R.id.total_price);
+        txtMsg1 = (TextView) findViewById(R.id.msg1);
+
+        NextProcessBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtTotalAmount.setText("Total Price = ₹"+(String.valueOf(overallTotalPrice)));
+                Intent intent = new Intent(CartActivity.this, ConfirmFinalOrderActivity.class);
+                intent.putExtra("Total Price", String.valueOf(overallTotalPrice));
+                startActivity(intent);
+                finish();
+            }
+        });
 
     }
 
     @Override
     protected void onStart() {
         super.onStart();
+        checkOrderStats();
         final DatabaseReference cartListRef = FirebaseDatabase.getInstance().getReference().child("Cart List");
         FirebaseRecyclerOptions<Cart> options = new FirebaseRecyclerOptions.Builder<Cart>()
                 .setQuery(cartListRef.child("Users View")
@@ -59,6 +77,8 @@ public class CartActivity extends AppCompatActivity {
                 holder.txtProductQuantity.setText("Quantity = "+model.getQuantity());
                 holder.txtProductPrice.setText("Price = "+model.getPrice()+" ₹");
                 holder.txtProductName.setText(model.getPname());
+                int oneTypeProductTprice = ((Integer.valueOf(model.getPrice())))*(Integer.valueOf(model.getQuantity()));
+                overallTotalPrice+=oneTypeProductTprice;
                 holder.itemView.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
@@ -124,5 +144,45 @@ public class CartActivity extends AppCompatActivity {
         recyclerView.setAdapter(adapter);
         adapter.startListening();
 
+    }
+    private void checkOrderStats()
+    {
+        DatabaseReference ordersRef;
+        ordersRef = FirebaseDatabase.getInstance().getReference().child("Orders").child(Prevalent.currentOnlineUser.getPhone());
+        ordersRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.exists())
+                {
+                    String shippingState = snapshot.child("state").getValue().toString();
+                    String userName  = snapshot.child("name").getValue().toString();
+                    if(shippingState.equals("shipped"))
+                    {
+                        txtTotalAmount.setText("Total Amount = ₹"+overallTotalPrice+"\n Dear "+userName+" \n order is shipped Successfully.");
+                        recyclerView.setVisibility(View.GONE);
+                        txtMsg1.setVisibility(View.VISIBLE);
+                        txtMsg1.setText("Congratulations, Your final order has been placed successfully. Soon, you will receive your order at your doorstep :) ");
+                        NextProcessBtn.setVisibility(View.GONE);
+                        Toast.makeText(CartActivity.this, "You can purchase more products once you received your first final order.", Toast.LENGTH_SHORT).show();
+
+                    }
+                    else if(shippingState.equals("not shipped"))
+                    {
+                        txtTotalAmount.setText("Shipping State = Not Shipped");
+                        recyclerView.setVisibility(View.GONE);
+                        txtMsg1.setVisibility(View.VISIBLE);
+                        NextProcessBtn.setVisibility(View.GONE);
+                        Toast.makeText(CartActivity.this, "You can purchase more products once you received your first final order.", Toast.LENGTH_SHORT).show();
+
+                    }
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/android/vocalforlocal/ProductDetailsActivity.java
+++ app/src/main/java/com/example/android/vocalforlocal/ProductDetailsActivity.java
@@ -35,7 +35,7 @@ public class ProductDetailsActivity extends AppCompatActivity {
     private ImageView productImage;
     private ElegantNumberButton numberButton;
     private TextView productPrice, productDescription, productName;
-    private String productID = "";
+    private String productID = "", state = "Normal";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,12 +53,26 @@ public class ProductDetailsActivity extends AppCompatActivity {
         AddToCartButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                addingToCartList();
+
+                
+                if(state.equals("Order Placed") || state.equals("Order Shipped"))
+                {
+                    Toast.makeText(ProductDetailsActivity.this, "You can purchase more products once your earlier products are shipped or confirmed", Toast.LENGTH_LONG).show();
+                }
+                else{
+                    addingToCartList();
+                }
             }
         });
 
 
     }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        checkOrderStats();
+    }
+
 
     private void addingToCartList() {
         String saveCurrentTime, saveCurrentDate;
@@ -122,6 +136,40 @@ public class ProductDetailsActivity extends AppCompatActivity {
                     productPrice.setText(products.getPrice());
                     productDescription.setText(products.getDescription());
                     Picasso.get().load(products.getImage()).into(productImage);
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+
+    private void checkOrderStats()
+    {
+        DatabaseReference ordersRef;
+        ordersRef = FirebaseDatabase.getInstance().getReference().child("Orders").child(Prevalent.currentOnlineUser.getPhone());
+        ordersRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.exists())
+                {
+                    String shippingState = snapshot.child("state").getValue().toString();
+                    String userName  = snapshot.child("name").getValue().toString();
+                    if(shippingState.equals("shipped"))
+                    {
+                        state = "Order Shipped";
+
+
+                    }
+                    else if(shippingState.equals("not shipped"))
+                    {
+                        state = "Order Placed";
+
+                    }
                 }
 
             }
