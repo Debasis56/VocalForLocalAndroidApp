be8513f3d86d0919dc8b2feddcee4b666ba5a099
Debasis56 <b519019@iiit-bh.ac.in>
2022-05-22 11:29:20 +0530

    Added Search Products Activity and Admin's feature to view and approve all customers' purchases.

--- app/src/main/java/com/example/android/vocalforlocal/AdminCategoryActivity.java
+++ app/src/main/java/com/example/android/vocalforlocal/AdminCategoryActivity.java
@@ -5,16 +5,39 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class AdminCategoryActivity extends AppCompatActivity {
 private ImageView bagsandwallets, broom, crafts, eatable, furnitures, handicrafts, handlooms, homeUtilities, jewelleryandhats, jutecrafts, kitchenanddining, officeutilities, terracotta, tribalpainting, woolenarts, stonecarvings;
-
+private Button LogoutBtn, CheckOrdersBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_admin_category);
+        LogoutBtn = (Button) findViewById(R.id.admin_logout_button);
+        CheckOrdersBtn = (Button) findViewById(R.id.check_orders_btn);
+
+        LogoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AdminCategoryActivity.this, MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+                finish();
+            }
+        });
+        CheckOrdersBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AdminCategoryActivity.this, AdminNewOrdersActivity.class);
+                 startActivity(intent);
+
+
+            }
+        });
         bagsandwallets = (ImageView)findViewById(R.id.BagsAndWallets);
         broom = (ImageView)findViewById(R.id.Brooms);
         crafts = (ImageView)findViewById(R.id.Crafts);
